a1df2a43179113a750ae53255cacc4794dac60b7
stnflp <stanciufilipsergiu4@gmail.com>
2023-03-24 19:24:17 +0200

    Remediated error

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -27,6 +27,8 @@ namespace TEMA_1_PAIRS
         public MainWindow()
         {
             InitializeComponent();
+            if (!File.Exists("database.txt")
+                File.Create("database.txt");
             showUsersList();
         }
 
